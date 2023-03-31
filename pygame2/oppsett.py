881c8691b59032b18d59625ac357692e40f6b35b
inevassgardeid <123069085+inevassgardeid@users.noreply.github.com>
2023-03-31 16:45:08 +0200

    oppdatert pygame

--- pygame2/oppsett.py
+++ pygame2/oppsett.py
@@ -10,7 +10,9 @@ clock = pygame.time.Clock()
 running = True
 
 polarbear = pygame.image.load("polarbear.png")
+polarbear = pygame.transform.scale(polarbear, (140,100))
 biden = pygame.image.load("biden.png")
+biden = pygame.transform.scale(biden, (100, 100))
 
 class Figur:
     def __init__(self, x, y):
@@ -25,38 +27,46 @@ class Spiller(Figur):
         super().__init__(x, y)
         self._fartY = fartY
 
-    def hopp(self):
-        self._y -= 15
-        self._x -= 4
+    def hopp(self): 
+        if 0 <= self._y: 
+            self._y -= 10
 
-    def fall(self):
-        self._y += 4
-        self._x += 2
+    def side_h(self): 
+        if self._x < 1280-140: 
+            self._x += 10 
 
-    def er_over_hinder(self):
-        pass
+    def side_v(self): 
+        if 0 <= self._x: 
+            self._x -= 10
+
+    def fall(self): 
+        if 720 - 100 > self._y: 
+            self._y += 4
+
 
 class Hinder(Figur):
-    def __init__(self, x, y, fartX, hoyde, bredde):
-        super().__init__(x, y)
-        self._fartX = fartX
-        self._hoyde = hoyde
-        self._bredde = bredde
+    def __init__(self, x, y, fartX, bredde): 
+        super().__init__(x, y) 
+        self._fartX = fartX 
+        self._hoyde = randint(20, 600) 
+        self._bredde = bredde 
+        self._y = 720 - self._hoyde
 
     def tegn(self, screen):
-        screen.blit(biden, (self._x,self._y))
+        screen.blit(biden, (self._x,self._y, self._bredde, self._hoyde))
 
     def flytt_venstre(self):
-        self._x += 1
+        self._x -= 2
 
 sirkel = Figur(30,100)
 spiller = Spiller(100,50,50)
+hindere = []
 
-hindere = [] 
-for i in range(3): 
-    height = randint(20,100)
-    nytt_hinder = Hinder(0, 100, 100, height, 10) 
-    hindere.append(nytt_hinder) 
+m = randint(1,12) 
+for i in range(m): 
+    b = randint(10,200) 
+    nytt_hinder = Hinder(1270, b, 100, 10) 
+    hindere.append(nytt_hinder)
 
 while running:
     # poll for events
@@ -66,7 +76,7 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill(140, 184, 255)
 
     # LAG SPILLET DIT HER:
     spiller.tegn()
@@ -78,7 +88,18 @@ while running:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
         spiller.hopp()
+    elif keys[pygame.K_LEFT]: 
+        spiller.side_v() 
+    elif keys[pygame.K_RIGHT]: 
+        spiller.side_h() 
     spiller.fall()
+
+    score = 0 
+    if spiller._y <= nytt_hinder._y and spiller._x <= nytt_hinder._x: 
+        score -= 10 
+    font = pygame.font.Font(None, 36) 
+    score_text = font.render(f'Score: {score}', True, (255, 255, 255)) 
+    screen.blit(score_text, (10, 10))
 
     # flip() the display to put your work on screen
     pygame.display.flip()
